93da8d6f46f2269085d9eabd241f6f8725fc8775
Jason Nemecek <jnemecek@gmail.com>
2019-09-23 14:44:06 -0500

    aaa

--- assets/js/app.js
+++ assets/js/app.js
@@ -418,8 +418,8 @@ var baseLayers = {
 
 var groupedOverlays = {
   "Points of Interest": {
-    "<img src='assets/img/marker-icon.png' width='24' height='28'>&nbsp;Theaters": theaterLayer,
-    "<img src='assets/img/museum.png' width='24' height='28'>&nbsp;Museums": museumLayer
+    "<img src='assets/img/marker-icon.png' width='24' height='28'>&nbsp;Theaters": theaterLayer /*,
+    "<img src='assets/img/museum.png' width='24' height='28'>&nbsp;Museums": museumLayer*/
   } /*,
   "Reference": {
     "Boroughs": boroughs,
