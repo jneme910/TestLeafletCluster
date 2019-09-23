5b9884720df9c10a1b14fb6ac49d7b4efe379cc1
Jason Nemecek <jnemecek@gmail.com>
2019-09-23 15:33:54 -0500

    z
    z

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,19 @@
 var map, featureList, boroughSearch = [], theaterSearch = [], museumSearch = [];
 
-$(window).resize(function() {
+/*$(window).resize(function() {
   sizeLayerControl();
-});
+});*/
 
 $(document).on("click", ".feature-row", function(e) {
   $(document).off("mouseout", ".feature-row", clearHighlight);
   sidebarClick(parseInt($(this).attr("id"), 10));
 });
 
-/*if ( !("ontouchstart" in window) ) {
+if ( !("ontouchstart" in window) ) {
   $(document).on("mouseover", ".feature-row", function(e) {
     highlight.clearLayers().addLayer(L.circleMarker([$(this).attr("lat"), $(this).attr("lng")], highlightStyle));
   });
-}*/
+}
 
 $(document).on("mouseout", ".feature-row", clearHighlight);
 
