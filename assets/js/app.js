a69829d365d03c06864642b6542e216d5925680f
Jason Nemecek <jnemecek@gmail.com>
2019-09-23 14:32:05 -0500

    s

--- assets/js/app.js
+++ assets/js/app.js
@@ -363,7 +363,7 @@ var attributionControl = L.control({
 });
 attributionControl.onAdd = function (map) {
   var div = L.DomUtil.create("div", "leaflet-control-attribution");
-  div.innerHTML = "<span class='hidden-xs'>Developed by <a href='http://bryanmcbride.com'>bryanmcbride.com</a> | </span><a href='#' onclick='$(\"#attributionModal\").modal(\"show\"); return false;'>Attribution</a>";
+  div.innerHTML = "<span class='hidden-xs'>Developed by <a href='https://github.com/jneme910/TestLeafletCluster'>Jason Nemecek</a> | </span><a href='#' onclick='$(\"#attributionModal\").modal(\"show\"); return false;'>Attribution</a>";
   return div;
 };
 map.addControl(attributionControl);
@@ -418,8 +418,8 @@ var baseLayers = {
 
 var groupedOverlays = {
   "Points of Interest": {
-    "<img src='assets/img/marker-icon.png' width='24' height='28'>&nbsp;Theaters": theaterLayer,
-    "<img src='assets/img/museum.png' width='24' height='28'>&nbsp;Museums": museumLayer
+    "<img src='assets/img/marker-icon.png' width='24' height='28'>&nbsp;Point": theaterLayer /*,*/
+   /* "<img src='assets/img/museum.png' width='24' height='28'>&nbsp;Museums": museumLayer */
   },
   "Reference": {
     "Boroughs": boroughs,
